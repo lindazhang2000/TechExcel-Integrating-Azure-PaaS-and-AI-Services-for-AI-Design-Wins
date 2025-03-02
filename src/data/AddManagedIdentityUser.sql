f9d603e63e2020a317143d11b565e500ce1779bf
lindazhang2000 <lindazhang2000@gmail.com>
2025-03-01 19:34:55 -0500

    ``` Refactor code structure for improved readability and maintainability ```

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER [id-o5qg7avyqjyeu]  FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER [id-o5qg7avyqjyeu];
+ALTER ROLE db_datawriter ADD MEMBER [id-o5qg7avyqjyeu] ;
 
